b343b080d8b34a59a0ad63abfa9fa2612ea1b2e9
Gustavo Cremaschi <gustavolimasilva.developer@gmail.com>
2026-04-13 13:02:14 -0300

    feat: adicionado recursos de imagem e o ícone do Google no layout de login

---
+++ app/src/main/java/br/com/fatec/book/tracker/ui/images/BookTrackerDrawableResources.kt
@@ -0,0 +1,17 @@
+package br.com.fatec.book.tracker.ui.images
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import br.com.fatec.book.tracker.R
+
+object BookTrackerDrawableResources {
+    object Images {
+        val imagemGoogle
+            @Composable
+            get() = painterResource(id = R.drawable.imagem_google)
+    }
+
+    object Vectors {
+
+    }
+}
